0217c5adac07db5600cf9436dcd767c702812d64
kareemthedeveloper <kareem.elsayed@smoothstack.com>
2021-03-05 10:19:25 -0500

    TestDataFactory and LWC to add members completed

---
+++ force-app/main/default/lwc/addMember/addMember.js
@@ -0,0 +1,52 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import MEMBER_OBJECT from '@salesforce/schema/Member__c';
+import NAME_FIELD from '@salesforce/schema/Member__c.Name';
+import DOB_FIELD from '@salesforce/schema/Member__c.Date_of_Birth__c';
+import ACTIVE_FIELD from '@salesforce/schema/Member__c.Active__c';
+import WEIGHT_FIELD from '@salesforce/schema/Member__c.Weight__c';
+import HEIGHTFT_FIELD from '@salesforce/schema/Member__c.Height_Feet__c';
+import HEIGHTIN_FIELD from '@salesforce/schema/Member__c.Height_Inches__c';
+import BACKSQUAT_FIELD from '@salesforce/schema/Member__c.Back_Squat__c';
+import DEADLIFT_FIELD from '@salesforce/schema/Member__c.Deadlift__c';
+import SHOULDERPRESS_FIELD from '@salesforce/schema/Member__c.Shoulder_Press__c';
+import EMAIL_FIELD from '@salesforce/schema/Member__c.Email__c';
+import FITNESSLEVEL_FIELD from '@salesforce/schema/Member__c.Fitness_Level__c';
+import TRAININGCLASS_FIELD from '@salesforce/schema/Member__c.Training_Class__c';
+
+
+
+
+
+export default class AddMember extends LightningElement {
+    memberObject = MEMBER_OBJECT;
+    nameField = NAME_FIELD;
+    dobField = DOB_FIELD;
+    activeField = ACTIVE_FIELD;
+    weightField = WEIGHT_FIELD;
+    heightFtField = HEIGHTFT_FIELD;
+    heightInField = HEIGHTIN_FIELD;
+    backSquatField = BACKSQUAT_FIELD;
+    deadliftField = DEADLIFT_FIELD;
+    shldPressField = SHOULDERPRESS_FIELD;
+    emailField = EMAIL_FIELD;
+    fitnessLevelField = FITNESSLEVEL_FIELD;
+    trainingClassField = TRAININGCLASS_FIELD;
+  
+
+
+     
+  showSuccessToast() {
+    const evt = new ShowToastEvent({
+        title: 'Toast Success',
+        message: 'Opearion sucessful',
+        variant: 'success',
+        mode: 'dismissable'
+    });
+    this.dispatchEvent(evt);
+    window.location.reload();
+     }
+    
+
+}
