d90d1c19c06180888dada33d043ece777f34d4b4
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2021-03-18 15:51:40 -0600

    Change to hide record a lift

--- force-app/main/default/lwc/displayWeather/displayWeather.js
+++ force-app/main/default/lwc/displayWeather/displayWeather.js
@@ -1,30 +1,29 @@
-import { LightningElement, api} from 'lwc';
-import performCallout from '@salesforce/apex/WebServiceLWC.performCallout';
+import { LightningElement } from "lwc";
+import performCallout from "@salesforce/apex/WebServiceLWC.performCallout";
 
 export default class displayWeather extends LightningElement {
-    @api recordId;
-    _weatherData;
-    _parsedWeatherData;
-    _name;
-    _temp;
-    _description;
-    _icon;
-    _time;
-    _convertedTemp;
-    
-    async connectedCallback(){
-        var d = new Date();
-        try{
-        this._weatherData = await performCallout({recordId: this.recordId});
-        this._parsedWeatherData = await JSON.parse(this._weatherData);
-        this._name = this._parsedWeatherData.name;
-        this._temp = this._parsedWeatherData.main.temp;
-        this._description = this._parsedWeatherData.weather[0].description;
-        this._icon = `http://openweathermap.org/img/wn/${this._parsedWeatherData.weather[0].icon}@2x.png`;
-        this._time = d.toLocaleTimeString();
-        this._convertedTemp = (this._temp * (9/5) + 32);
-        }catch(error){
-            console.log(error);
-        }
+  _weatherData;
+  _parsedWeatherData;
+  _name;
+  _temp;
+  _description;
+  _icon;
+  _time;
+  _convertedTemp;
+
+  async connectedCallback() {
+    var d = new Date();
+    try {
+      this._weatherData = await performCallout();
+      this._parsedWeatherData = await JSON.parse(this._weatherData);
+      this._name = this._parsedWeatherData.name;
+      this._temp = this._parsedWeatherData.main.temp;
+      this._description = this._parsedWeatherData.weather[0].description;
+      this._icon = `http://openweathermap.org/img/wn/${this._parsedWeatherData.weather[0].icon}@2x.png`;
+      this._time = d.toLocaleTimeString();
+      this._convertedTemp = this._temp * (9 / 5) + 32;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
