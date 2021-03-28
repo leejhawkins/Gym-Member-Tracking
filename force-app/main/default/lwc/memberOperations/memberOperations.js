0643e76157cfa4299698dae7d63e9c4a2a77a594
Lee Hawkins <59981819+leejhawkins@users.noreply.github.com>
2021-03-28 15:59:07 -0600

    Revert "Forecast component displays dates"

---
+++ force-app/main/default/lwc/memberOperations/memberOperations.js
@@ -0,0 +1,30 @@
+import { LightningElement, wire } from "lwc";
+import findMembers from "@salesforce/apex/MemberController.findMembers";
+import { publish, MessageContext } from "lightning/messageService";
+import RECORD_SELECTED_CHANNEL from "@salesforce/messageChannel/Record_Selected__c";
+const DELAY = 100;
+
+export default class MemberOperations extends LightningElement {
+  searchKey = "";
+
+  @wire(findMembers, { searchKey: "$searchKey" }) members;
+  @wire(MessageContext)
+  messageContext;
+
+  handleContactSelect(event) {
+    event.preventDefault();
+    const payload = { recordId: event.target.member.Id };
+    publish(this.messageContext, RECORD_SELECTED_CHANNEL, payload);
+  }
+
+  handleKeyChange(event) {
+    // Debouncing this method: Do not update the reactive property as long as this function is
+    // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
+    window.clearTimeout(this.delayTimeout);
+    const searchKey = event.target.value;
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    this.delayTimeout = setTimeout(() => {
+      this.searchKey = searchKey;
+    }, DELAY);
+  }
+}
