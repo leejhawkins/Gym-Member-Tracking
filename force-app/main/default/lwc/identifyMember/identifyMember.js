899e00f7635ebe566e14bf950e85d9bbcb2773b9
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2021-06-29 09:40:34 -0600

    Changes to get communities to work

--- force-app/main/default/lwc/identifyMember/identifyMember.js
+++ force-app/main/default/lwc/identifyMember/identifyMember.js
@@ -3,7 +3,6 @@ import Id from "@salesforce/user/Id";
 import getMember from "@salesforce/apex/ContactController.getMember";
 
 export default class IdentifyMember extends LightningElement {
-  
   userId = Id;
   recordid;
   memberName;
@@ -15,14 +14,15 @@ export default class IdentifyMember extends LightningElement {
       .then((data) => {
         this.recordid = data.Id;
         this.memberName = data.Name;
+        console.log(this.memberName);
       })
       .catch((error) => {
         this.error = error;
+        console.log(this.error);
       });
   }
   handleRecordScore(event) {
     this.recordid = null;
     this.recordid = event.detail.id;
   }
-
 }
